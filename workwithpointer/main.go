fbceb65eadac551f23eca7cea7f97ebaf852830d
Kira1108 <327485253@163.com>
2021-09-20 01:31:55 +0800

    add pointers

--- workwithpointer/main.go
+++ workwithpointer/main.go
@@ -1,5 +1,11 @@
 package main
 
+// Value Types: int, float, string, bool, structs
+// use pointer to change things in a function
+
+// Reference Types: slice, map, channel, pointer, function
+// don't worry about pointers with this
+
 import "fmt"
 
 type Person struct {
@@ -31,6 +37,10 @@ func (p *Person) updateSuperEasy(firstname string) {
 	p.firstname = firstname
 }
 
+func udpateSlice(s []string) {
+	s[0] = "bye"
+}
+
 func main() {
 	wanghuan := Person{firstname: "Wang", lastname: "Huan"}
 	wanghuan.print()
@@ -60,4 +70,12 @@ func main() {
 	// 这就说明我们不需要取出一个东西的地址
 	// 我们也不需要从一个pointertype中取出这个地址指向的东西
 	// 一切简简单单，就能成功
+
+	// But update slice works
+	// 你把这个someSlice传给updateSlicefunction的时候，这个slice还是copy了
+	// 但是copy过后的这个slice，和someSlice还是指向了同一个底层array
+	// 所以这时候更改这个值就成功了
+	someSlice := []string{"hi", "there", "how", "are", "you"}
+	udpateSlice(someSlice)
+	fmt.Println("\n", someSlice)
 }
